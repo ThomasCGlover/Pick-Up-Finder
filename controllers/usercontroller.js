48df08541462a4b1485228ba90c8278b29ea4bdf
ThomasCGlover <thomascodyglover@gmail.com>
2021-07-10 16:58:03 -0400

    ready for deployment

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -16,7 +16,7 @@ router.post("/register", async (req, res) => {
             role
         });
 
-        const token = jwt.sign({id: newUser.id, username: newUser.username}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24})
+        const token = jwt.sign({id: newUser.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24})
 
         res.status(201).json({
             message: "User successfully registered!",
